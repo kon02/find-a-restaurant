e0a5fedb66d99db529222d116719a4f614174b1c
kon02 <kotahigasi@gmail.com>
2023-07-22 11:52:44 +0900

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\PostRequest; // useする
+
+class PostController extends Controller
+{
+    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        //getPaginateByLimit()はPost.phpで定義したメソッド
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Post $post, PostRequest $request)//PostRequestで条件を満たさないと保存できないようにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        // $post->title = $input["title"];
+        // $post->save;
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
+}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <body>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <div class="content">
+            <div class="content__post">
+                <h3>本文</h3>
+                <p>{{ $post->body }}</p>    
+            </div>
+        </div>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">編集</a>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+    
+</x-app-layout>
